c1f32ef418b70020d502ebdab32942640e5a81aa
stella <lorenzo>
2020-06-12 10:55:34 +0200

    update material-ui dependency, fix fetch data on App.js and .gitignore

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Content from './components/Content'
 import WindowDimensionsProvider from './components/WindowDimensionsProvider'
-import data_old from './data.json'
-import data from './components/Firebase/fb-function.js'
+import firebase from "./components/Firebase/firebase";
 
-const App = () => {
-    return (
-        <WindowDimensionsProvider>
-            <Content items={data}/>
-        </WindowDimensionsProvider>
-    );
+const db = firebase.database();
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {items: []}
+    }
+
+    componentDidMount() {
+        db.ref("/data").on("value", snapshotData => {
+            let allItems = [];
+            snapshotData.forEach(snapshot => {
+                allItems.push(snapshot.val())
+            })
+            this.setState({items: allItems})
+        })
+    }
+
+    render() {
+        return (
+            <WindowDimensionsProvider>
+                <Content items={this.state.items}/>
+            </WindowDimensionsProvider>
+        );
+    }
 }
 
 export default App
